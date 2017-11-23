0ad48595d51644221bdd7b1ae1e30f3f679e3f7c
Nolan Byrnes <nolanehs@gmail.com>
2017-11-23 15:55:21 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -6,8 +6,8 @@ def initialize(title)
   @add_backer = []
 end
 
-def add_backer(backers)
-@add_backer << backers
+def add_backer(backer)
+@add_backer << backer
 backer.backed_projects << self unless backer.backed_projects.include?(self)
 end
 
