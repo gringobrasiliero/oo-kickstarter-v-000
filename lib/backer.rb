4a141332cd7b9dfee415ff2d932e3876693e34d9
Nolan Byrnes <nolanehs@gmail.com>
2017-11-23 15:54:02 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,7 @@ end
 
 def back_project(awesome_project)
 @backed_projects << awesome_project
-project.backers << self unless project.backers.include?(self)
+Project.backers << self unless project.backers.include?(self)
 end
 
 end
