dff26cd0fa7800d7a350ba13ae16719c940b0011
Nolan Byrnes <nolanehs@gmail.com>
2017-11-23 15:46:19 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,14 @@
 class Backer
-attr_accessor :name
+attr_accessor :name, :backed_projects
 
 def initialize(name)
   @name=name
-
+@backed_projects = []
 end
-@@backed_projects = []
+
+
 def back_project(awesome_project)
-@@backed_projects << awesome_project
+@backed_projects << awesome_project
 end
 
 end
